3b0a70ac9e2ece3b68a873477e993803b32a1174
husky <huskyhacks.mk@gmail.com>
2024-01-13 10:54:45 -0500

    debug info and readme

--- README.md
+++ README.md
@@ -1,10 +1,42 @@
 # clarion
 The clarion call tells you if someone is logging into an AitM proxy that is proxying your M365 login page
 
-
-
 ## Setup
+For testing, you can use a self-signed certificate. Make sure you accept the warning about the invalid certificate in the browser that you want to test.
 ```
 openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
 ```
 
+For production use, you would want a legitimate signed certificate.
+
+Clone the repo to your publically accessible host and install the requirements:
+
+```
+$ pip3 install -r requirements.txt
+```
+
+Then run the app (you need root level permissions to bind to port 443):
+
+```
+$ [sudo] python3 app.py
+
+ * Serving Flask app 'app'
+ * Debug mode: on
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on all addresses (0.0.0.0)
+ * Running on https://127.0.0.1:443
+ * Running on https://[Public IP]:443
+Press CTRL+C to quit
+ * Restarting with stat
+
+[*] Your public IP address is: [Public IP]
+[*] Embed this pixel in your CSS file with the following code:
+
+body {
+    background-image: url('https://[Public IP]/[pixel name].png');
+    background-size: 0 0;
+}
+
+ * Debugger is active!
+ * Debugger PIN: 422-205-484
+```
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, send_file
 import os
 import uuid
+import datetime
 from PIL import Image
 from io import BytesIO
 import requests
@@ -35,13 +36,15 @@ def get_public_ip():
 # Route to serve the pixel image
 @app.route(f'/{pixel_filename}')
 def pixel():
-    # Identify the requesting host
-    requester = request.remote_addr
+    # Capture the IP address of the requester
+    requester_ip = request.remote_addr
+    referer_header = request.headers.get('Referer')
+    if 'login.microsoftonline.com' not in referer_header:
+        print(f"{Fore.YELLOW}[!] Non-Microsoft referer header detected: {requester_ip}{Style.RESET_ALL}")
+        # Print debug information
+        print(f"[*] Debug Information:")
+        print(f"[*] Requester IP: {requester_ip}")
 
-    # Warn if the requester is not from microsoft.com
-    # Note: This is a simplistic check and may not be reliable for all cases
-    if 'microsoft.com' not in requester:
-        app.logger.warning(f"Non-Microsoft host detected: {requester}")
 
     # Serve the pixel image
     return send_file(create_pixel(), mimetype='image/png')
@@ -53,13 +56,13 @@ def main():
     else:
         public_ip = get_public_ip()
         if public_ip:
+            print()
             print(f"{Fore.CYAN}[*] Your public IP address is:{Style.RESET_ALL} {Fore.GREEN}{public_ip}{Style.RESET_ALL}")
             print(f"{Fore.CYAN}[*] Embed this pixel in your CSS file with the following code:{Style.RESET_ALL}\n")
-            print(f"{Fore.MAGENTA}body {{")
+            print(f"{Fore.MAGENTA}[some CSS element] {{")
             print(f"{Fore.MAGENTA}    background-image: url('https://{public_ip}/{pixel_filename}');")
             print(f"{Fore.MAGENTA}    background-size: 0 0;")
             print(Fore.MAGENTA + "}" + Style.RESET_ALL)
-            
             print()
 
             app.run(ssl_context=('cert.pem', 'key.pem'), host='0.0.0.0', port=443, debug=True)
